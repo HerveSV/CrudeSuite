cc1e641907eaf9f25a3076181090a21bb9050cf0
Hervé <herve.schmitveiler@icloud.com>
2020-04-25 18:56:24 +0800

    Initial Commit

---
+++ CrudeEngine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CrudeEngine
+//
+//  Created by Hervé Schmit-Veiler on 4/25/20.
+//  Copyright © 2020 Hervé Schmit-Veiler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
