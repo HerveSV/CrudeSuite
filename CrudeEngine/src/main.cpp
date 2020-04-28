f78198a5d8a3acb9de593518beacd2b961f61b62
Hervé <herve.schmitveiler@icloud.com>
2020-04-28 23:02:11 +0800

    imgui not working yet

--- CrudeEngine/src/main.cpp
+++ CrudeEngine/src/main.cpp
@@ -8,9 +8,18 @@
 
 #include <iostream>
 #include <cstdint>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+
 #include <cmath>
+#include <glad/glad.h>
+//#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "vendor/imgui/imgui.h"
+#include "vendor/imgui/imgui_impl_glfw.h"
+#include "vendor/imgui/imgui_impl_opengl3.h"
+#include <stdio.h>
+
+
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -44,6 +53,7 @@ int main()
 
     const size_t bufferWidth = 760;
     const size_t bufferHeight = 500;
+    const char* glsl_version = "#version 130";
     Renderer renderer;
     renderer.initOpenGL(3, 3, GLFW_OPENGL_CORE_PROFILE, GL_TRUE);
     renderer.createWindow(bufferWidth, bufferHeight, "cookie&dummy");
@@ -236,19 +246,84 @@ int main()
     texture1.bind(0);
     texture2.bind(2);
     
+    /*
+     ImGUI setup
+     */
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+    
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(renderer.getWindow(), true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+    /*
+     */
     
 
-
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     
     renderer.setClearColour(0.2f, 0.3f, 0.3f, 1.0f);
     renderer.enableDepthTest();
     while(!renderer.windowShouldClose())
     {
-        
+        processInput(renderer.getWindow());
         renderer.clearColourBuffer();
         renderer.clearDeptBuffer();
+        
+        ImGui_ImplOpenGL3_NewFrame();
+      //  ImGui_ImplGlfw_NewFrame();
+      //  ImGui::NewFrame();
+        
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+        
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+            
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+            
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+            
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+            
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+            
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+        
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
+        
+        // Rendering
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());/**/
+
         //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //turn off wireframe mode
-        processInput(renderer.getWindow());
+        
         shaderProgram.bind();
 //        shaderProgram.setVec3f("uColour", sin(glfwGetTime()), sin(glfwGetTime()+glm::radians(30.0f)), sin((glfwGetTime())+glm::radians(60.0f)));
         shaderProgram.setMat4("model", model);
